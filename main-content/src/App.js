8a9b3a41e36117d6b721fc09aafed29c680e4f73
user <jongwoop97@gmail.com>
2023-05-16 15:58:38 +0900

    part 1c

--- main-content/src/App.js
+++ main-content/src/App.js
@@ -1,15 +1,35 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick = {props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+  const decreaseByOne = () => setCounter(counter - 1);
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter = {counter}/>
+      <Button handleClick = {increaseByOne} text = {'plus'} />
+      <Button handleClick = {setToZero} text = {'zero'} />
+      <Button handleClick = {decreaseByOne} text = {'minus'} />
+      
+    </div>
   )
 }
 
