ac0a6b0ccb03ac8fd7cd47e2fd934c6bf1d448fd
user <jongwoop97@gmail.com>
2023-06-01 00:59:39 +0900

    exercise 2.8: add numbers to phonebook:

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,23 +1,29 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1 }
+  const [persons, setPersons] = useState([{ 
+    id: 1 ,
+    name: 'Arto Hellas', 
+    number: "010-1234-5678"
+  }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
     console.log(persons.findIndex(person => person.name));
     if (persons.findIndex(person => person.name === newName) !== -1) {
       window.alert(`${newName} is already added to phonebook`);
     } else {     
       const personObject = {
+        id: persons.length + 1,
         name: newName,
-        id: persons.length + 1
+        number: newNumber
       }
       setPersons(persons.concat(personObject));
       setNewName('');
+      setNewNumber('');
     }
   }
 
@@ -25,13 +31,17 @@ const App = () => {
     setNewName(event.target.value);
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit" >add</button>
         </div>
@@ -39,7 +49,7 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map((person) => {
         return(
-          <div key={person.id}>{person.name}</div>
+          <div key={person.id}>{person.name} {person.number}</div>
         )
       })}
     </div>
