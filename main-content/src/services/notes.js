99ec3665cc1ccd147d9104a8cf221f211e7e5013
user <jongwoop97@gmail.com>
2023-06-05 17:46:27 +0900

    part 2d: done

--- main-content/src/services/notes.js
+++ main-content/src/services/notes.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 const baseUrl = 'http://localhost:3001/notes';
 
+// const getAll = () => {
+//     const request = axios.get(baseUrl);
+//     return request.then(response => response.data);
+// }
+
 const getAll = () => {
-    const request = axios.get(baseUrl);
-    return request.then(response => response.data);
-}
+    const request = axios.get(baseUrl)
+    const nonExisting = {
+      id: 10000,
+      content: 'This note is not saved to server',
+      important: true,
+    }
+    return request.then(response => response.data.concat(nonExisting))
+  }
 
 const create = newObject => {
     const request =  axios.post(baseUrl, newObject);
@@ -16,8 +26,4 @@ const update = (id, newObject) => {
     return request.then(response => response.data);
 }
 
-export default {
-    getAll: getAll,
-    create: create,
-    update: update
-}
+export default { getAll, create, update }
