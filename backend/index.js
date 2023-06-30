ee84c7f6bed90aefcfd3137181db246717058fa9
user <jongwoop97@gmail.com>
2023-06-30 12:19:48 +0900

    part 3b - continue from (the whole app to the internet)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+app.use(express.static('build'));
+
+const cors = require('cors');
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -88,7 +92,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT ,() => {
   console.log(`Server running on port ${PORT}`);
 });
